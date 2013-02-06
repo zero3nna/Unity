eadf695d0c68d1dab67cfff9d936784b701286a7
Steve Maahs <gleichda@abwesend.de>
2013-02-06 22:00:58 +0100

    change playerpref and highscore

--- Assets/Scripts/Steve_scripts/GUI/GUISetPlayerPrefs.js
+++ Assets/Scripts/Steve_scripts/GUI/GUISetPlayerPrefs.js
@@ -3,35 +3,36 @@
 function Start () {
 	if(!(PlayerPrefs.GetInt("Init"))){
 		PlayerPrefs.SetInt("Init",1);
+		Debug.Log("in INIT Pref");
 		
-		PlayerPrefs.SetString("HPLayerName0","Name0");
-		PlayerPrefs.SetInt("HPLayerValue0",0);
-		PlayerPrefs.SetString("HPLayerName1","Name1");
-		PlayerPrefs.SetInt("HPLayerValue1",0);
-		PlayerPrefs.SetString("HPLayerName2","Name2");
-		PlayerPrefs.SetInt("HPLayerValue2",0);
-		PlayerPrefs.SetString("HPLayerName3","Name3");
-		PlayerPrefs.SetInt("HPLayerValue3",0);
-		PlayerPrefs.SetString("HPLayerName4","Name4");
-		PlayerPrefs.SetInt("HPLayerValue4",0);
-		PlayerPrefs.SetString("HPLayerName5","Name5");
-		PlayerPrefs.SetInt("HPLayerValue5",0);
-		PlayerPrefs.SetString("HPLayerName6","Name6");
-		PlayerPrefs.SetInt("HPLayerValue6",0);
-		PlayerPrefs.SetString("HPLayerName7","Name7");
-		PlayerPrefs.SetInt("HPLayerValue7",0);
-		PlayerPrefs.SetString("HPLayerName8","Name8");
-		PlayerPrefs.SetInt("HPLayerValue8",0);
+		PlayerPrefs.SetString("HPlayerName0","Name0");
+		PlayerPrefs.SetInt("HPlayerValue0",0);
+		PlayerPrefs.SetString("HPlayerName1","Name1");
+		PlayerPrefs.SetInt("HPlayerValue1",0);
+		PlayerPrefs.SetString("HPlayerName2","Name2");
+		PlayerPrefs.SetInt("HPlayerValue2",0);
+		PlayerPrefs.SetString("HPlayerName3","Name3");
+		PlayerPrefs.SetInt("HPlayerValue3",0);
+		PlayerPrefs.SetString("HPlayerName4","Name4");
+		PlayerPrefs.SetInt("HPlayerValue4",0);
+		PlayerPrefs.SetString("HPlayerName5","Name5");
+		PlayerPrefs.SetInt("HPlayerValue5",0);
+		PlayerPrefs.SetString("HPlayerName6","Name6");
+		PlayerPrefs.SetInt("HPlayerValue6",0);
+		PlayerPrefs.SetString("HPlayerName7","Name7");
+		PlayerPrefs.SetInt("HPlayerValue7",0);
+		PlayerPrefs.SetString("HPlayerName8","Name8");
+		PlayerPrefs.SetInt("HPlayerValue8",0);
 		
 		PlayerPrefs.SetInt("isGaming",0);
 		PlayerPrefs.SetInt("currentKillCount",0);
+		
+		PlayerPrefs.SetInt("AmmoCount",0);
+		PlayerPrefs.SetInt("AmmoPackCount",0);
+		PlayerPrefs.SetInt("ActiveWeapon",0);
+		
+		PlayerPrefs.SetInt("HealthPoints",0);
 	}
-	if(!(PlayerPrefs.GetInt("CurrentKillCount"))){
-		PlayerPrefs.SetInt("CurrentKillCount",20);
-	}else{
-	Debug.Log(PlayerPrefs.GetInt("CurrentKillCount"));
-	}
-	
 	
 	NotificationCenter.DefaultCenter().AddObserver(this, "GameOver");
 	NotificationCenter.DefaultCenter().AddObserver(this, "ResetAll");
