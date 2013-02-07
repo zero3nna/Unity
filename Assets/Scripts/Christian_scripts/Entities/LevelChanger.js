6cd706932730172d35adcf75d354ee567e91ae4b
Christian <make.panic@googlemail.com>
2013-02-07 01:47:56 +0100

    added levelchanger fadein/fadeout

--- Assets/Scripts/Christian_scripts/Entities/LevelChanger.js
+++ Assets/Scripts/Christian_scripts/Entities/LevelChanger.js
@@ -1,20 +1,41 @@
 #pragma strict
 var levelName : String;
+var blackTexture : Texture;
+var alpha : float = 1;
+
+private var fadeout : boolean = false;
+private var fadein : boolean = false;
 
 function Start () {
-
+	
 }
 
 function Update () {
+	if(fadeout){
+		alpha -= Mathf.Clamp01(Time.deltaTime / 5);
+		if(alpha < 0){
+			fadeout = false;
+			Application.LoadLevel(levelName);
+		}
+	}else if(fadein){
+		alpha += Mathf.Clamp01(Time.deltaTime / 5);
+		if(alpha > 1){
+			fadein = false;
+			Application.LoadLevel(levelName);
+		}
+	}
+}
 
+function OnGUI(){
+	GUI.color = new Color(0, 0, 0, alpha);
+	GUI.DrawTexture( new Rect(0, 0, Screen.width, Screen.height ), blackTexture );
 }
 
 function OnTriggerEnter (other : Collider) {
 	if(other.tag == "Player"){
 		//Player entered
-		
 		NotificationCenter.DefaultCenter().PostNotification(this, "ChangeLevel",0);
-		Application.LoadLevel(levelName);
+		fadein = true;
 	}
-    Debug.Log(other.tag);
+    Debug.Log("TAG=" + other.tag);
 }
